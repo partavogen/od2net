98a17ee878e02d39be58229ec2e78cec2a13909f
Daniel Silva <146825303+partavogen@users.noreply.github.com>
2023-12-21 04:41:55 +0000

    fix bugs

--- examples/lisbon/run_od2net-lisbon.sh
+++ examples/lisbon/run_od2net-lisbon.sh
@@ -2,14 +2,10 @@
 script bashLogs_od2net-lisbon_1stTestRun.txt
 
 ### run od2net-lisbon (direct)
-cd ~/Documents/daniel/od2net
-cargo run --release examples/lisbon/config_direct.json
+cd ~/Documents/daniel/od2net/examples/lisbon
+python3 setup.py
+cargo run --release config_direct.json
 
-
-cd ~/Documents/daniel/od2net/exam
-cargo run --release examples/lisbon/config-direct.json
-
-cargo help run
 
 
 ### run od2net-lisbon (quiet)
